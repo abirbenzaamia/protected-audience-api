4a3721ea302f7f626bbe518adbdf55e70b59e19c
Abir BENZAAMIA <abirbenzaamia@Mhds-MacBook-Pro.local>
2024-06-19 11:50:39 +0200

    fix permission for join ad interest group

--- advertiser/src/App.js
+++ advertiser/src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      {/* <iframe allow='join-ad-interest-group' title='test' className='dsp-pixel' src='https://protected-audience-api-dsp.onrender.com'>
-      </iframe> */}
+      <iframe allow='join-ad-interest-group' title='test' height="0" width="0" className='dsp-pixel' src='https://protected-audience-api-dsp.onrender.com'>
+      </iframe>
+      
       <h2>Advertiser Product Page</h2>
       <p>Here is the products</p>
       </header>
