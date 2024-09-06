c8ac8e7b386972f22c0f6aa5385655f1e32b637e
Abir BENZAAMIA <abirbenzaamia@223-69.wifi-inria-saclay.saclay.inria.fr>
2024-09-06 11:31:34 +0200

    deploy ad site

--- adtechservice/server.js
+++ adtechservice/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const port = 3000
+const port = 8080
+const handlebars = require('express-handlebars');
+
 
 app.get('/', (req, res) => {
   res.send('Welcome the The DSP ')
@@ -13,7 +15,23 @@ app.use(express.static('./ads'))
 
 app.use(cors());
 
+//Sets our app to use the handlebars engine
+// app.set('view engine', 'handlebars');
+//Sets handlebars configurations (we will go through them later on)
+// app.engine('handlebars', handlebars({
+// layoutsDir: __dirname + '/views',
+// }));
 
+//app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+
+res.render('main', {layout : 'index'});
+});
+
+
+// add attestation files 
 
 app.get('/.well-known/privacy-sandbox-attestations.json', (req, res) => {
   res.json({
