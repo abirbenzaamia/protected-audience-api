e759e3f9283ce569abb175fcc4763eaacd68b87f
Abir BENZAAMIA <abirbenzaamia@223-69.wifi-inria-saclay.saclay.inria.fr>
2024-09-06 15:48:29 +0200

    add dsp url in ad site

--- adtechservice/public/src/join-interest-group.js
+++ adtechservice/public/src/join-interest-group.js
@@ -167,15 +167,15 @@ var interestGroup = {
 
     // This should be the origin where this file will be hosted
     // This should be the same origin as biddingLogicUrl below
-    owner: "https://protected-audience-api-advertiser.onrender.com",
+    owner: "https://protected-audience-api-tsc2.onrender.com",
 
     // This script must have the same origin as the owner directly above
     // An example of bidding logic is provided in the next section.
-    biddingLogicUrl: "https://protected-audience-api-advertiser.onrender.com/bidding-logic.js",
+    biddingLogicUrl: "https://protected-audience-api-tsc2.onrender.com/src/bidding-logic.js",
     // joiningOrigin : "https://protected-audience-api-advertiser.onrender.com",
     // Protected Audience API requires that any ad that could be returned by bidding logic is listed here.
     // For this example there is only one ad which you will set up in Step 4.2
-    joiningOrigin:"https://protected-audience-api-advertiser.onrender.com",
+    joiningOrigin:"https://shoes-store-i4te.onrender.",
     ads: [{
         renderUrl: "https://protected-audience-api-advertiser.onrender.com/ad.html",
     }],
