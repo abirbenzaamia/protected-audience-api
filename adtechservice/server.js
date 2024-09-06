a6cf80230f63dd9d7a14cd38701462aef6d4afd8
Abir BENZAAMIA <abirbenzaamia@223-69.wifi-inria-saclay.saclay.inria.fr>
2024-09-06 13:43:19 +0200

    add hbs

--- adtechservice/server.js
+++ adtechservice/server.js
@@ -1,73 +1,29 @@
+// import { create } from 'express-handlebars'
+
 const express = require('express')
+const { engine } = require('express-handlebars');
+
 const app = express()
 const cors = require('cors');
 const port = 8080
-const handlebars = require('express-handlebars');
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome the The DSP ')
-})
 
 // Serve decision-logic.js and run-ad-auction.js files
 app.use(express.static('./src'))
 app.use(express.static('./ads'))
-
 app.use(cors());
 
-//Sets our app to use the handlebars engine
-// app.set('view engine', 'handlebars');
-//Sets handlebars configurations (we will go through them later on)
-// app.engine('handlebars', handlebars({
-// layoutsDir: __dirname + '/views',
-// }));
+// Configure handlebars
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', './views')
 
-//app.use(express.static('public'))
-
+// run the app 
 app.get('/', (req, res) => {
-//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-
-res.render('main', {layout : 'index'});
-});
+  res.render('main', {layout : 'index'});
+})
 
 
-// add attestation files 
 
-app.get('/.well-known/privacy-sandbox-attestations.json', (req, res) => {
-  res.json({
-  "privacy_sandbox_api_attestations": [
-    {
-      "attestation_parser_version": "2",
-      "attestation_version": "1",
-      "privacy_policy": [],
-      "ownership_token": "q2KA5o3efBQVJoZo13gt7JLG6w326siWv22j4kRbkClPYEyJ5TTojdaw6JYUUehr",
-      "issued_seconds_since_epoch": 1720451668,
-      "enrollment_id": "5FUNX",
-      "enrollment_site": "https://protected-audience-api-advertiser.onrender.com/",
-      "platform_attestations": [
-        {
-          "platform": "chrome",
-          "attestations": {
-            "attribution_reporting_api": {
-              "ServiceNotUsedForIdentifyingUserAcrossSites": true
-            },
-            "topics_api": {
-              "ServiceNotUsedForIdentifyingUserAcrossSites": true
-            },
-            "protected_audience_api": {
-              "ServiceNotUsedForIdentifyingUserAcrossSites": true
-            }
-          }
-        },
-        {
-          "platform": "android",
-          "attestations": {}
-        }
-      ]
-    }
-  ]
-});
-});
 
 app.get('/.well-known/interest-group/permissions', (req, res) => {
   res.json({
