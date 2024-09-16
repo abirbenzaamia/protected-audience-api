abc977a13c143c58264b7cf1b9f2bc5f73ec9538
Abir BENZAAMIA <abirbenzaamia@222-176.wifi-inria-saclay.saclay.inria.fr>
2024-09-16 18:06:10 +0200

    fix permission policy

--- publisher/src/components/AdSlot.jsx
+++ publisher/src/components/AdSlot.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function AdSlot() {
   return (
-    <iframe title='3rd party ad content' allow='Ad-Auction-Allowed: true' src='https://protected-audience-api-tsc2.onrender.com/ssp/'> 
+    <iframe title='3rd party ad content' allow='run-ad-auction *' src='https://protected-audience-api-tsc2.onrender.com/ssp/'> 
     The winnning ad
     </iframe>
   )
