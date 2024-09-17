a596a18be19b50bbfbb1e4244864782b81abfb56
Abir BENZAAMIA <abirbenzaamia@222-147.wifi-inria-saclay.saclay.inria.fr>
2024-09-17 16:29:40 +0200

    fix issues with permission policy

--- adtechservice/server.js
+++ adtechservice/server.js
@@ -37,19 +37,18 @@ app.get('/dsp', (req, res) => {
 app.get('/ssp', (req, res) => {
   res.render('adAuction', {layout : 'index'});
 })
-app.get('/ssp/decision-logic.js ', (req, res) => {
-  res.render('decisionLogic', {layout : 'index'});
-})
-
 
 //permission policy for run ad auction 
-
 app.use((req, res, next) => {
   res.set('Permissions-Policy', 'run-ad-auction=*');
   // Proceed to the next middleware or route handler
   next();
 });
 
+app.use('/ssp/decision-logic.js', (req, res, next) => {
+  res.setHeader('Ad-Auction-Allowed', 'true');
+  next();
+});
 
 app.get('/.well-known/interest-group/permissions', (req, res) => {
   res.json({
