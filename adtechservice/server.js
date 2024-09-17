a80497b274716d308c01c3f3a7b3d3490cfb27e7
Abir BENZAAMIA <abirbenzaamia@222-147.wifi-inria-saclay.saclay.inria.fr>
2024-09-17 18:19:19 +0200

    permission policy

--- adtechservice/server.js
+++ adtechservice/server.js
@@ -36,11 +36,11 @@ app.get('/ssp', (req, res) => {
 })
 
 // permission policy for run ad auction 
-// app.use('/',(req, res, next) => {
-//   res.set('Permissions-Policy', 'run-ad-auction=*');
-//   // Proceed to the next middleware or route handler
-//   next();
-// });
+app.use('/',(req, res, next) => {
+  res.set('Permissions-Policy', 'run-ad-auction=*');
+  // Proceed to the next middleware or route handler
+  next();
+});
 
 
 
