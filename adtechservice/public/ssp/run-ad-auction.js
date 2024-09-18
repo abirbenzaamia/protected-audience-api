1e7c2c4008070e7d64f60271d540f1029bed6368
Abir BENZAAMIA <abirbenzaamia@222-147.wifi-inria-saclay.saclay.inria.fr>
2024-09-18 13:24:29 +0200

    run ad auction

--- adtechservice/public/ssp/run-ad-auction.js
+++ adtechservice/public/ssp/run-ad-auction.js
@@ -1,29 +1,30 @@
+const auctionConfig = {
+  // This should be the same origin as decisionLogicUrl below
+  seller: "https://protected-audience-api-tsc2.onrender.com", 
+  decisionLogicUrl: "https://protected-audience-api-tsc2.onrender.com/ssp/decision-logic.js",
 
-async function runAuction() {
-  const resolveToConfig = typeof window.FencedFrameConfig !== 'undefined';
+  // This should be a list of all DSPs that you wish to participate in this auction
+  interestGroupBuyers: ['https://protected-audience-api-tsc2.onrender.com'],
+// This object will be available to all auction participants
+  auctionSignals: { },
 
-  const auctionConfig = {
-    seller: 'https://protected-audience-api-tsc2.onrender.com/',
-    decisionLogicUrl: `https://protected-audience-api-tsc2.onrender.com/ssp/decision-logic.js`,
-    interestGroupBuyers: ['https://protected-audience-api-tsc2.onrender.com/'],
-    perBuyerSignals: {
-      ['https://protected-audience-api-tsc2.onrender.com/']: { windowInnerHeight: window.innerHeight },
-    },
-    resolveToConfig
-  };
- 
-  // Run ad auction
-  const selectedAd = await navigator.runAdAuction(auctionConfig);
-  console.log(selectedAd)
-  // Render ad
-  const frame = document.getElementById('protected-audience-ad');
+  // This object will only be available to the seller during scoreAd function
+  sellerSignals: { },
 
-  if (resolveToConfig && selectedAd instanceof FencedFrameConfig) {
-    frame.config = selectedAd;
-  } else {
-    frame.src = selectedAd;
-  }
+  // This parameter is optional, and is only available to the Buyer of each object. This is data the seller explicitly provides to individual buyers.
+  // Each key in this object should match an entry in the interestGroupBuyers array above
+  perBuyerSignals: {
+  "https://protected-audience-api-tsc2.onrender.com": { },
+},
 };
 
+async function runAuction(){
+// Run ad auction
+const opaqueUrl = await navigator.runAdAuction(auctionConfig);
+// Render ad
+const iframeEl = document.createElement('iframe');
+iframeEl.src = opaqueUrl;
+document.body.appendChild(iframeEl);
+}
 
 runAuction();
