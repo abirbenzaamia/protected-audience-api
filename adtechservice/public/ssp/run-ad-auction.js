260aafb5a6a95990c34231e1f048a4c309bbed7b
Abir BENZAAMIA <abirbenzaamia@222-8.wifi-inria-saclay.saclay.inria.fr>
2024-09-19 11:21:17 +0200

    display winning ad

--- adtechservice/public/ssp/run-ad-auction.js
+++ adtechservice/public/ssp/run-ad-auction.js
@@ -1,62 +1,74 @@
-// const auctionConfig = {
-//   // This should be the same origin as decisionLogicUrl below
-//   seller: "https://protected-audience-api-tsc2.onrender.com", 
-//   decisionLogicUrl: "https://protected-audience-api-tsc2.onrender.com/ssp/decision-logic.js",
 
-//   // This should be a list of all DSPs that you wish to participate in this auction
-//   interestGroupBuyers: ['https://protected-audience-api-tsc2.onrender.com', 'https://td.doubleclick.net', 'https://f.creativecdn.com', 'https://fledge.eu.criteo.com'],
-// // This object will be available to all auction participants
-//   auctionSignals: { },
+// const runAuction = async () => {
+//   const resolveToConfig = typeof window.FencedFrameConfig !== 'undefined';
 
-//   // This object will only be available to the seller during scoreAd function
-//   sellerSignals: { },
-
-//   // This parameter is optional, and is only available to the Buyer of each object. This is data the seller explicitly provides to individual buyers.
-//   // Each key in this object should match an entry in the interestGroupBuyers array above
-//   perBuyerSignals: {
-//   "https://protected-audience-api-tsc2.onrender.com": { },
-// },
-// perBuyerTimeouts: {
-//   '*': 50,
-// }
-// };
-const runAuction = async () => {
-  const resolveToConfig = typeof window.FencedFrameConfig !== 'undefined';
-
-  const auctionConfig = {
-   // This should be the same origin as decisionLogicUrl below
-   seller: "https://protected-audience-api-tsc2.onrender.com", 
-   decisionLogicURL: "https://protected-audience-api-tsc2.onrender.com/ssp/decision-logic.js",
+//   const auctionConfig = {
+//    // This should be the same origin as decisionLogicUrl below
+//    seller: "https://protected-audience-api-tsc2.onrender.com", 
+//    decisionLogicURL: "https://protected-audience-api-tsc2.onrender.com/ssp/decision-logic.js",
  
-   // This should be a list of all DSPs that you wish to participate in this auction
-   interestGroupBuyers: ['https://protected-audience-api-tsc2.onrender.com'],
- // This object will be available to all auction participants
-    auctionSignals: { isControversial: true },
-    sellerSignals: { key: 'value' },
-    sellerTimeout: 100,
-    perBuyerSignals: {
-      ['https://protected-audience-api-tsc2.onrender.com']: { windowInnerHeight: window.innerHeight },
-    },
-    perBuyerTimeouts: {
-      '*': 50,
-    },
-    resolveToConfig
-  };
+//    // This should be a list of all DSPs that you wish to participate in this auction
+//    interestGroupBuyers: ['https://protected-audience-api-tsc2.onrender.com', 'https://protected-audience-demo-dsp.web.app'],
+//  // This object will be available to all auction participants
+//     auctionSignals: { isControversial: true },
+//     sellerSignals: { key: 'value' },
+//     sellerTimeout: 100,
+//     perBuyerSignals: {
+//       ['https://protected-audience-api-tsc2.onrender.com']: { windowInnerHeight: window.innerHeight },
+//     },
+//     perBuyerTimeouts: {
+//       '*': 50,
+//     },
+//     resolveToConfig
+//   };
 
-  console.log('auctionConfig = ', JSON.stringify(auctionConfig));
+//   console.log('auctionConfig = ', JSON.stringify(auctionConfig));
 
-  // Run ad auction
-  const selectedAd = await navigator.runAdAuction(auctionConfig);
-  console.log(selectedAd)
-  // Render ad
-  const frame = document.getElementById('protected-audience-ad');
+//   // Run ad auction
+//   const selectedAd = await navigator.runAdAuction(auctionConfig);
+//   console.log(selectedAd)
 
-  if (resolveToConfig && selectedAd instanceof FencedFrameConfig) {
-    frame.config = selectedAd;
-  } else {
-    frame.src = selectedAd;
-  }
-};
+//   // Render ad
+//   const frame = document.getElementById('protected-audience-ad');
+
+//   if (resolveToConfig && selectedAd instanceof FencedFrameConfig) {
+//     frame.config = selectedAd;
+//   } else {
+//     frame.src = selectedAd;
+//   }
+// };
+
+//runAuction();
+
+//const resolveToConfig = typeof window.FencedFrameConfig !== 'undefined';
+const auctionConfig = {
+  // This should be the same origin as decisionLogicUrl below
+  seller: "https://protected-audience-api-tsc2.onrender.com", 
+  decisionLogicURL: "https://protected-audience-api-tsc2.onrender.com/ssp/decision-logic.js",
+
+  // This should be a list of all DSPs that you wish to participate in this auction
+  interestGroupBuyers: ['https://protected-audience-api-tsc2.onrender.com', 'https://protected-audience-demo-dsp.web.app'],
+// This object will be available to all auction participants
+   auctionSignals: { isControversial: true },
+   sellerSignals: { key: 'value' },
+   sellerTimeout: 100,
+   perBuyerSignals: {
+     ['https://protected-audience-api-tsc2.onrender.com']: { windowInnerHeight: window.innerHeight },
+   },
+   perBuyerTimeouts: {
+     '*': 50,
+   },
+ };
+
+async function runAuction(){
+// Run ad auction
+const opaqueUrl = await navigator.runAdAuction(auctionConfig);
+// Render ad
+console.log(opaqueUrl)
+const iframeEl = document.createElement('iframe');
+iframeEl.src = opaqueUrl;
+document.body.appendChild(iframeEl);
+}
 
 runAuction();
 
--- publisher/src/components/AdSlot.jsx
+++ publisher/src/components/AdSlot.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AdSlot() {
   return (
     <iframe title='3rd party ad content' allow='run-ad-auction' src='https://protected-audience-api-tsc2.onrender.com/ssp/'>
-    <fencedframe id='protected-audience-ad' mode='opaque-ads'></fencedframe>
+    {/* <fencedframe id='protected-audience-ad' mode='opaque-ads'></fencedframe> */}
     </iframe>
   )
 }
